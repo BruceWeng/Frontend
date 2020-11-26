5e1fcb5a3d2bd6ceeb683541c796bc0bc31acddd
Bruce Weng <weng8916@gmail.com>
2020-11-25 21:03:37 -0800

    23. Create a sum()

---
+++ 23. Create a sum().js
@@ -0,0 +1,12 @@
+function sum(num) {
+  function partialSum(nextNum) {
+    return nextNum ? sum(num + nextNum) : num;
+  }
+  /*
+    In order to get the result of sum(1)(2)(3)...(N), there are 2 options:
+    1. Put an empty parantheses at the end - sum(1)(2)(3)() (Preferred in a real world scenario)
+    2. Use sum(1)(2)(3).valueOf() (Used for testing the solution of this problem)
+  */
+  partialSum.valueOf = () => num;
+  return partialSum;
+}
