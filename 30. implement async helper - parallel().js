6303eaae22cb21c8f058d254e306f999d7bf1b37
Bruce Weng <weng8916@gmail.com>
2020-12-09 14:09:22 -0800

    30. Implement aync helper - parallel

---
+++ 30. implement async helper - parallel().js
@@ -0,0 +1,39 @@
+/*
+type Callback = (error: Error, data: any) => void
+
+type AsyncFunc = (
+  callback: Callback,
+  data: any
+) => void
+
+*/
+
+/**
+ * @param {AsyncFunc[]} funcs
+ * @return {(callback: Callback) => void}
+ */
+function parallel(funcs) {
+  // your code here
+  return function(callback, input) {
+    promiseAll(funcs.map(func => makePromise(func, input)))
+    .then(data => callback(undefined, data))
+    .catch(err => callback(err, input))
+  }
+}
+
+function promiseAll(promises) {
+  return promises.reduce((prev, next) => {
+    return prev
+    .then(res => next.then(data => [...res, data]),
+          err => Promise.reject(err))
+  }, Promise.resolve([]));
+}
+
+const makePromise = (func, arg) => {
+  return new Promise((resolve, reject) => {
+    func((err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    }, arg);
+  })
+}
