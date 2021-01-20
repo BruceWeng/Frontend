80389d62c6625480f5785662b3d2e6f534b821ef
Bruce Weng <weng8916@gmail.com>
2021-01-19 22:18:33 -0800

    curry()

--- High Frequency/1. Implement curry().js
+++ High Frequency/1. Implement curry().js
@@ -6,11 +6,25 @@ function curry(func) {
     // 1. if enough args, call func
     // 2. if not enough, bind the args and wait for new one
 
-    if (args.length >= func.length) {
-      return func.apply(this, args);
+    if (args.length >= func.length) { // function.length return how many args it expects
+      return func.apply(this, args); // execute func only when args.length >= func.length
     } else {
       // 1,2
       return curried.bind(this, ...args);
     }
   };
 }
+
+const join = (a, b, c) => {
+  return `${a}_${b}_${c}`;
+}
+
+const curriedJoin = curry(join);
+
+console.log(curriedJoin(1, 2, 3)); // '1_2_3'
+
+console.log(curriedJoin(1)(2, 3)); // '1_2_3'
+
+console.log(curriedJoin(1, 2)(3)); // '1_2_3'
+
+console.log(curriedJoin(1, 2, 3, 4)); // '1_2_3'
