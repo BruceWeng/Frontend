3b64ca22ecfa1a341feb00c391f36d6dc08a7760
Bruce Weng <weng8916@gmail.com>
2021-01-20 14:52:40 -0800

    reduce()

---
+++ High Frequency/2. Implement Array.prototype.reduce().js
@@ -0,0 +1,19 @@
+/**
+ * Write a function reduce(func: function, initialState: any): any
+ * behaves the same as Array.prototype.reduce().
+ */
+Array.prototype.myreduce = function (func, initialValue) {
+  let result = initialValue;
+  for (const current of this) { // 'this' is the array calls myreduce
+    result = func(result, current);
+  }
+  return result;
+}
+
+let initialValue = 0
+let sum = [{x: 1}, {x: 2}, {x: 3}].myreduce(
+    (accumulator, currentValue) => accumulator + currentValue.x
+    , initialValue
+)
+
+console.log(sum) // 6
