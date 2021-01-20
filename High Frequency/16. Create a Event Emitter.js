d85fd389dd4cdc50ef2325bbb3356179f041dfb6
Bruce Weng <weng8916@gmail.com>
2021-01-19 21:58:05 -0800

    16. Event Emitter

--- High Frequency/16. Create a Event Emitter.js
+++ High Frequency/16. Create a Event Emitter.js
@@ -19,3 +19,22 @@ class EventEmitter {
     }
   }
 }
+const emitter = new EventEmitter();
+const sum = (a, b) => {
+  console.log(a + b);
+}
+const sub1  = emitter.subscribe('event1', sum)
+const sub2 = emitter.subscribe('event2', sum)
+
+// same callback could subscribe 
+// on same event multiple times
+const sub3 = emitter.subscribe('event1', sum)
+
+emitter.emit('event1', 1, 2); // 3 3
+// callback1 will be called twice
+
+sub1.release()
+sub3.release()
+// now even if we emit 'event1' again, 
+// callback1 is not called anymore
+emitter.emit('event1', 1, 2); // no value
