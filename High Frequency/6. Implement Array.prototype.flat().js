e9cf2539d0ceb22cda4140bf6be14da0de1024bc
Bruce Weng <weng8916@gmail.com>
2021-02-09 12:46:40 -0800

    6. Implement Array.prototype.flat()

--- High Frequency/6. Implement Array.prototype.flat().js
+++ High Frequency/6. Implement Array.prototype.flat().js
@@ -11,6 +11,20 @@ console.log(flat([[1,1], [[2],1], [[3, [4]],1]], Infinity)); // [1, 1, 2, 1, 3, 
  * @param {Array} arr
  * @param {number} depth
  */
+// Recursion
+function flat(arr, depth=1) {
+  let result = [];
+  if (arr.length === 0) return result;
+  for (const current of arr) { // current = array | number
+    if (Array.isArray(current) && depth > 0) {
+      result = [...result, ...flat(current, depth-1)];
+    } else {
+      result.push(current);
+    }
+  }
+  return result;
+}
+
 // Iteration
 function flat(arr, depth=1) {
   const result = [];
@@ -24,18 +38,4 @@ function flat(arr, depth=1) {
     }
   }
   return result.reverse();
-}
-
-// Recursion
-function flat(arr, depth=1) {
-  let result = [];
-  if (arr.length === 0) return result;
-  for (const current of arr) {
-    if (Array.isArray(current) && depth > 0) {
-      result = [...result, ...flat(current, depth-1)];
-    } else {
-      result.push(current);
-    }
-  }
-  return result;
 }
