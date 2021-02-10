e88175c584bb45b8fa1226797914953cae0d44f8
Bruce Weng <weng8916@gmail.com>
2021-02-09 16:12:01 -0800

    11. Next Right Sibling

--- High Frequency/11. Next Right Sibling.js
+++ High Frequency/11. Next Right Sibling.js
@@ -4,19 +4,17 @@
  * @return {HTMLElemnt | null}
  */
 function nextRightSibling(root, target) {
-  if (root === null) {
-    return null;
-  }
-  const queue = [root, null];
-  while (queue.length > 0) {
-    const node = queue.shift();
-    if (node === target) {
-      return queue.shift();
-    } else if (node === null && queue.length !== 0) {
-      queue.push(null);
-    } else {
+  const queue = [root];
+  while (queue.length !== 0) {
+    let size = queue.length;
+    while (size > 0) {
+      const node = queue.shift();
+      if (node === target) {
+        if (size === 1) return null;
+        else return queue.shift();
+      }
       queue.push(...node.children);
+      size--;
     }
   }
-  return null;
 }
