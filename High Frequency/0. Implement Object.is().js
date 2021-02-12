d7efaf87f3f433d3d1776da0aa4aeb246aa9025b
Bruce Weng <weng8916@gmail.com>
2021-02-11 18:02:07 -0800

    0. Implement Object.is()

---
+++ High Frequency/0. Implement Object.is().js
@@ -0,0 +1,20 @@
+/**
+ * Object.is() is similar to === except following cases
+ * 
+ * Object.is(0, -0) // false
+ * 0 === -0 // true
+ * 
+ * Object.is(NaN, NaN) // true
+ * NaN === NaN // false
+ */
+
+/**
+ * @param {any} a
+ * @param {any} b
+ * @return {boolean}
+ */
+function is(a, b) {
+  if (a === 0 && b === 0) return (1 / a === 1 / b);
+  if (Number.isNaN(a) && Number.isNaN(b)) return true;
+  return a === b;
+}
