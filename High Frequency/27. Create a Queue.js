b00ef1e9a19b676f290cffc53f5c6c987d5a6063
Bruce Weng <weng8916@gmail.com>
2021-02-17 14:51:26 -0800

    27. Queue

---
+++ High Frequency/27. Create a Queue.js
@@ -0,0 +1,37 @@
+class Q {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  peek() {
+    return this.head;
+  }
+
+  enqueue(node) {
+    const new_node = { node, next: null };
+    if (this.head === null) this.head = this.tail = new_node;
+    else {
+      this.tail.next = new_node;
+      this.tail = new_node;
+    }
+    this.size++;
+    return this.size;
+  }
+
+  dequeue() {
+    if (this.head === null) return null;
+    const current = this.head;
+    this.head = this.head.next;
+    this.size--;
+    return current.node;
+  }
+}
+
+const queue = new Q();
+queue.enqueue(1);
+queue.enqueue(2);
+console.log(queue.size); // 2
+console.log(queue.dequeue()); // 1
+console.log(queue.size); 1
