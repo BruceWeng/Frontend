b800fdc802be7217633927756c312f57aa79bdcb
Bruce Weng <weng8916@gmail.com>
2021-02-18 21:28:59 -0800

    29. BST

---
+++ High Frequency/29. Create a Binary Search Tree.js
@@ -0,0 +1,63 @@
+// Assume all the keys are unique.
+class TreeNode {
+  constructor(key) {
+    this.left = null;
+    this.right = null;
+    this.key = key;
+  }
+}
+function insertNode(node, key) {
+  if (node === null) {
+    node = new TreeNode(key);
+    return node;
+  }
+  if (key < node.key) node.left = insertNode(node.left, key);
+  else node.right = insertNode(node.right, key);
+  return node;
+}
+
+function inorder(node, result=[]) {
+  if (node.left !== null) inorder(node.left, result);
+  result.push(node.key);
+  if (node.right !== null) inorder(node.right, result);
+  return result;
+}
+
+function minValueNode(node) {
+  if (node.left === null) return node;
+  minValueNode(node.left);
+}
+
+function deleteNode(node, key) {
+  if (node === null) return node;
+  if (key < node.key) node.left = deleteNode(node.left, key);
+  if (key > node.key) node.right= deleteNode(node.right, key);
+  if (key === node.key) {
+    if (node.left === null) {
+      let new_node = node.right;
+      node = null;
+      return new_node;
+    }
+    if (node.right === null) {
+      let new_node = node.left;
+      node = null;
+      return new_node;
+    }
+    // If both children are not null
+    // Replace inorder successor (next larger node of target) key to this node 
+    let successor = minValueNode(node.right);
+    node.key = successor.key;
+    // Delete inorder successor 
+    node.right = deleteNode(node.right, successor.key);
+  }
+  return node;
+}
+
+let root = new TreeNode(1);
+root = insertNode(root, 4);
+root = insertNode(root, 3);
+root = insertNode(root, 2);
+console.log(inorder(root)); // [1,2,3,4]
+console.log(minValueNode(root)); // node 1
+root = deleteNode(root, 3);
+console.log(inorder(root)); // [1,2,4]
