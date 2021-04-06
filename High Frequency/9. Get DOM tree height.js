f34154c72e2bb0e3ba064bc5f62c8da48f0253f9
Bruce Weng <weng8916@gmail.com>
2021-04-06 15:25:17 -0700

    9. Get DOM tree height shorten

--- High Frequency/9. Get DOM tree height.js
+++ High Frequency/9. Get DOM tree height.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 function getHeight(tree) {
-  if (tree === null) return 0;
+  if(tree===null) return 0;
   let maxHeight = 0;
-  for (let child of tree.children) {
+  for(let child of tree.children) {
     maxHeight = Math.max(maxHeight, getHeight(child));
   }
-  return 1 + maxHeight;
+  return 1+maxHeight;
 }
 
 // Iteration solution
@@ -17,13 +17,13 @@ function getHeight(tree) {
  * @returns {number}
  */
 function getHeight(tree) {
-  if (tree === null) return 0;
+  if(tree===null) return 0;
   let height = 0;
   const q = [tree];
-  while (q.length > 0) {
+  while(q.length>0) {
     let size = q.length;
     height++;
-    while (size > 0) {
+    while(size>0) {
       const head = q.shift()
       q.push(...head.children)
       size--;
@@ -38,7 +38,7 @@ function getHeight(tree) {
  */
 function getHeight(tree) {
   // your code here         
-  if (tree === null) return 0;
-  if (tree.children.length === 0) return 1;
+  if(tree===null) return 0;
+  if(tree.children.length===0) return 1;
   return Math.max(...[...tree.children].map((item) => getHeight(item)))+1;
 }
