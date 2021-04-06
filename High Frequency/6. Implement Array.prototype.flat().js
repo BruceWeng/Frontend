bd42b09ed1170a08d9704415870ddd94e86c0b35
Bruce Weng <weng8916@gmail.com>
2021-04-06 14:55:06 -0700

    6. Implement Array.prototype.flat() shorten

--- High Frequency/6. Implement Array.prototype.flat().js
+++ High Frequency/6. Implement Array.prototype.flat().js
@@ -14,13 +14,10 @@ console.log(flat([[1,1], [[2],1], [[3, [4]],1]], Infinity)); // [1, 1, 2, 1, 3, 
 // Recursion
 function flat(arr, depth=1) {
   let result = [];
-  if (arr.length === 0) return result;
-  for (const current of arr) { // current = array | number
-    if (Array.isArray(current) && depth > 0) {
-      result = [...result, ...flat(current, depth-1)];
-    } else {
-      result.push(current);
-    }
+  if(arr.length===0) return result;
+  for(const current of arr) { // current = array | number
+    if(Array.isArray(current) && depth>0) result = [...result, ...flat(current, depth-1)];
+    else result.push(current);
   }
   return result;
 }
@@ -29,13 +26,10 @@ function flat(arr, depth=1) {
 function flat(arr, depth=1) {
   const result = [];
   const stack = [...arr.map(item => [item, depth])];
-  while (stack.length !== 0) {
+  while(stack.length!==0) {
     const [current, depth] = stack.pop(); // current = array | number
-    if (Array.isArray(current) && depth > 0) {
-      stack.push(...current.map(item => [item, depth-1]));
-    } else {
-      result.push(current);
-    }
+    if(Array.isArray(current) && depth>0) stack.push(...current.map(item => [item, depth-1]));
+    else result.push(current);
   }
   return result.reverse();
 }
