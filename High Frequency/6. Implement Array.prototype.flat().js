478abbe1c4fa300566adb38489982a1fd363ca0f
Bruce Weng <weng8916@gmail.com>
2021-04-06 15:00:27 -0700

    6. Implement Array.prototype.flat() shorten

--- High Frequency/6. Implement Array.prototype.flat().js
+++ High Frequency/6. Implement Array.prototype.flat().js
@@ -16,8 +16,11 @@ function flat(arr, depth=1) {
   let result = [];
   if(arr.length===0) return result;
   for(const current of arr) { // current = array | number
-    if(Array.isArray(current) && depth>0) result = [...result, ...flat(current, depth-1)];
-    else result.push(current);
+    if(Array.isArray(current) && depth>0) {
+      result = [...result, ...flat(current, depth-1)];
+      continue
+    }
+    result.push(current);
   }
   return result;
 }
@@ -28,8 +31,11 @@ function flat(arr, depth=1) {
   const stack = [...arr.map(item => [item, depth])];
   while(stack.length!==0) {
     const [current, depth] = stack.pop(); // current = array | number
-    if(Array.isArray(current) && depth>0) stack.push(...current.map(item => [item, depth-1]));
-    else result.push(current);
+    if(Array.isArray(current) && depth>0) {
+      stack.push(...current.map(item => [item, depth-1]));
+      continue
+    }
+    result.push(current);
   }
   return result.reverse();
 }
