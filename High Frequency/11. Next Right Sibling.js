26e4448ab714fb56b614c42de8d772d6c0d70b09
Bruce Weng <weng8916@gmail.com>
2021-04-06 21:57:02 -0700

    11. Next Right Sibling shorten

--- High Frequency/11. Next Right Sibling.js
+++ High Frequency/11. Next Right Sibling.js
@@ -5,14 +5,12 @@
  */
 function nextRightSibling(root, target) {
   const queue = [root];
-  while (queue.length !== 0) {
+  while(queue.length!==0) {
     let size = queue.length;
-    while (size > 0) {
+    while(size>0) {
       const node = queue.shift();
-      if (node === target) {
-        if (size === 1) return null;
-        else return queue.shift();
-      }
+      if(node===target && size===1) return null
+      if(node===target) return queue.shift();
       queue.push(...node.children);
       size--;
     }
