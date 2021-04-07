4508e9013e6c85ceb62b9272475c2642d328fb5d
Bruce Weng <weng8916@gmail.com>
2021-04-06 21:54:29 -0700

    10. get DOM tags shorten

--- High Frequency/10. get DOM tags.js
+++ High Frequency/10. get DOM tags.js
@@ -10,7 +10,7 @@ function getTags(tree) {
 
 function visit(tree, set) {
   set.add(tree.tagName.toLowerCase());
-  for (let child of tree.children) visit(child, set);
+  for(let child of tree.children) visit(child, set);
 }
 
 // Iteration solution:DFS
@@ -21,7 +21,7 @@ function visit(tree, set) {
 function getTags(tree) {
   const set = new Set()
   const stack = [tree]
-  while (stack.length > 0) {
+  while(stack.length>0) {
     const top = stack.pop()
     set.add(top.tagName.toLowerCase())
     stack.push(...top.children)
@@ -32,12 +32,12 @@ function getTags(tree) {
 // BFS
 function getTags(tree) {
   // your code here
-  if (tree === null) return [];
+  if(tree===null) return [];
   const set = new Set();
   const queue = [tree];
-  while (queue.length !== 0) {
+  while(queue.length!==0) {
     let size = queue.length;
-    while (size > 0) {
+    while(size>0) {
       let node = queue.shift();
       set.add(node.tagName.toLowerCase());
       queue.push(...node.children);
