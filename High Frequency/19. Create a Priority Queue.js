c5b67ce67fdb6ed4cde51b6a1498c6231fdd1728
Bruce Weng <weng8916@gmail.com>
2021-04-07 17:49:48 -0700

    19. Create a Priority Queue shorten

--- High Frequency/19. Create a Priority Queue.js
+++ High Frequency/19. Create a Priority Queue.js
@@ -1,12 +1,12 @@
 class PriorityQueue {
-  constructor(compareFunc=(a, b) => a - b) { // min heap by default
+  constructor(compareFunc=(a, b) => a-b) { // min heap by default
     this.heap = [];
     this.compare = compareFunc;
     // Utility
     this.top = 0;
-    this.parent = i => ((i + 1) >> 1) - 1; // for shiftUp
-    this.left = i => (i << 1) + 1; // for shiftDown
-    this.right = i => (i << 1) + 2; // for shiftDown
+    this.parent = i => ((i+1)>>1)-1; // for shiftUp
+    this.left = i => (i<<1)+1; // for shiftDown
+    this.right = i => (i<<1)+2; // for shiftDown
   }
   size() {
     return this.heap.length;
@@ -22,7 +22,7 @@ class PriorityQueue {
     return this.size();
   }
   poll() {
-    if (this.size() === 0) return;
+    if(this.size()===0) return;
     let root = this.heap[this.top];
     this.heap[this.top] = this.heap[this.size()-1];
     this.heap.pop();
@@ -31,33 +31,33 @@ class PriorityQueue {
   }
   // Helper functions
   more(a, b) {
-    return this.compare(this.heap[a], this.heap[b]) < 0;
+    return this.compare(this.heap[a], this.heap[b])<0;
   }
   swap(a, b) {
     [ this.heap[a], this.heap[b] ] = [ this.heap[b], this.heap[a] ];
   }
   siftUp(index) {
-    while (index > 0) {
+    while(index>0) {
       let parent_index = this.parent(index);
-      if (this.more(parent_index, index)) break;
+      if(this.more(parent_index, index)) break;
       this.swap(parent_index, index);
       index = parent_index;
     }
   }
   siftDown(index) {
-    while (true) {
+    while(true) {
       let swap_index = null;
       let right_index = this.right(index);
       let left_index = this.left(index);
 
-      if (left_index < this.size() 
+      if(left_index<this.size() 
         && this.more(left_index, index)) swap_index = left_index;
 
-      if (right_index < this.size() 
+      if(right_index<this.size() 
         && this.more(right_index, index) 
         && this.more(right_index, left_index)) swap_index = right_index;
 
-      if (swap_index === null) break;
+      if(swap_index===null) break;
 
       this.swap(swap_index, index);
       index = swap_index;
