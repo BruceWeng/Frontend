0705ad79a9ad7712e311b0caa152fc5f8c12c245
Bruce Weng <weng8916@gmail.com>
2021-04-14 13:09:23 -0700

    23. Implement lowerBound

--- High Frequency/23. Implement lowerBound.js
+++ High Frequency/23. Implement lowerBound.js
@@ -4,6 +4,7 @@ console.log(search([3, 5, 7, 11, 13, 17, 19], 6)); // false
 console.log(lowerBound([3, 5, 7, 11, 13, 17, 19], 7)); // 11
 console.log(lowerBound([3, 5, 7, 11, 13, 17, 19], 6)); // 7
 console.log(lowerBound([3, 5, 7, 11, 13, 17, 19], 19)); // null
+console.log(lowerBound([3, 5, 7, 11, 13, 17, 19], 2)); // 3
 
 function search(space, target) {
   let left = 0, right = space.length-1; // right = N - 1
@@ -16,14 +17,23 @@ function search(space, target) {
   return false;
 }
 
+/**
+ * right = space.length && left = mid+1:
+ * [1, 2, 3, 4]
+ *        ^
+ *        |      
+ *       mid
+ * 1. edge case: left === space.length
+ * 2. edge case: left === 0
+ */
 function lowerBound(space, target) {
-  let left = 0, right = space.length-1; // right = N - 1
+  let left = 0, right = space.length; // right = N
   while(left<right) { // left < right
     let mid = left+((right-left)>>1);
     if(space[mid]>target) right = mid;
     if(space[mid]<=target) left = mid+1;
   }
   // space[left] is the last value and less and equal than target
-  if(space[left]<=target) return null
+  if(left===space.length) return null
   return space[left];
 }
