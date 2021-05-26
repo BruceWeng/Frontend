e6b7ba489bd9538482a5a40133f10f5fb6135e08
Bruce Weng <weng8916@gmail.com>
2021-05-26 15:16:24 -0700

    update details

--- High Frequency/18.2 MyPromise-v4.js
+++ High Frequency/18.2 MyPromise-v4.js
@@ -5,11 +5,11 @@ class MyPromise {
     REJECTED: 2
   }
   constructor(executor) {
-    this.state = this.STATE.PENDING;
+    this.state = this.STATE.PENDING
     try {
-      executor(this._resolve.bind(this), this._reject.bind(this));
+      executor(this._resolve.bind(this), this._reject.bind(this))
     } catch (error) {
-      this._reject(error);
+      this._reject(error)
     }
   }
 
@@ -21,25 +21,23 @@ class MyPromise {
     // assign a callback return value to class variable if onFulfilled is not a function
     this.onFulfilled = typeof onFulfilled === 'function'
       ? onFulfilled 
-      : (value) => value;
+      : (value) => value
     // assign a callback throw error to class variable if onRejected is not a function
     this.onRejected = typeof onRejected === 'function'
       ? onRejected
-      : (error) => {
-          throw error;
-        };
+      : (value) => {throw value}
     return new MyPromise((resolve, reject) => {
       // Register `resolve` and `reject`, so that we can
       // resolve or reject this promise in `_resolve`
       // or `_reject`.
-      this.thenResolve = resolve;
-      this.thenReject = reject;
+      this.thenResolve = resolve
+      this.thenReject = reject
     });
   }
 
   // Consumer
   catch(onRejected) {
-    return this.then(undefined, onRejected);
+    return this.then(undefined, onRejected)
   }
 
   // Consumer
@@ -49,17 +47,17 @@ class MyPromise {
     // onFinally should not alter the rejection reason
     return this.then(
       (value) => Promise.resolve(onFinally()).then(() => value), 
-      (error) => Promise.resolve(onFinally()).then(() => Promise.reject(error))
-    );
+      (value) => Promise.resolve(onFinally()).then(() => Promise.reject(value))
+    )
   }
 
   // Producer
   _resolve(value) {
     // only handle pending promise
-    if (this.state !== this.STATE.PENDING) return;
+    if (this.state !== this.STATE.PENDING) return
     // change state and result
-    this.state = this.STATE.FULFILLED;
-    this.result = value;
+    this.state = this.STATE.FULFILLED
+    this.result = value
     // in _resolve: returnValue is this.onFulfilled(this.result)
     // in queueMicrotask: 
     // in try:
@@ -68,38 +66,37 @@ class MyPromise {
     // in catch (error):
     // this.thenReject(error)
     queueMicrotask(() => {
-      if (this.onFulfilled === undefined) return;
+      if (this.onFulfilled === undefined) return
       try {
-        const returnValue = this.onFulfilled(this.result);
+        const returnValue = this.onFulfilled(this.result)
         if (returnValue instanceof MyPromise) {
-          returnValue.then(this.thenResolve, this.thenReject);
+          returnValue.then(this.thenResolve, this.thenReject)
         } else {
-          this.thenResolve(returnValue);
+          this.thenResolve(returnValue)
         }
       } catch (error) {
-        this.thenReject(error);
+        this.thenReject(error)
       }
     });
   }
 
   // Producer
-  _reject(error) {
-    if (this.state !== this.STATE.PENDING) return;
-    this.state = this.STATE.REJECTED;
-    this.result = error;
-    // in _reject: returnValue is this.onRejected(this.result)
+  // only 3 lines different from _resolve
+  _reject(value) {
+    if (this.state !== this.STATE.PENDING) return
+    this.state = this.STATE.REJECTED // (1)
+    this.result = value
     queueMicrotask(() => {
-      if (this.onRejected === undefined) return;
+      if (this.onRejected === undefined) return // (2)
       try {
-        const returnValue = this.onRejected(this.result);
-        const isReturnValuePromise = returnValue instanceof MyPromise;
+        const returnValue = this.onRejected(this.result) // (3)
         if (returnValue instanceof MyPromise) {
-          returnValue.then(this.thenResolve, this.thenReject);
+          returnValue.then(this.thenResolve, this.thenReject)
         } else {
-          this.thenResolve(returnValue);
+          this.thenResolve(returnValue)
         }
       } catch (error) {
-        this.thenReject(error);
+        this.thenReject(error)
       }
     });
   }
@@ -107,10 +104,10 @@ class MyPromise {
   static resolve(value) {
     return (value instanceof MyPromise) 
       ? value 
-      : new MyPromise((resolve) => resolve(value));
+      : new MyPromise((resolve) => resolve(value))
   }
 
   static reject(value) {
-    return new MyPromise((_, reject) => reject(value));
+    return new MyPromise((_, reject) => reject(value))
   }
 }
